cd9dcd014eac5413eb597b83c83464c012e7f2ca
Antonio Guzman <aguzmanv09@gmail.com>
2021-09-10 11:14:53 -0500

    Create appbar cart

--- src/frontend/components/Appbar/index.tsx
+++ src/frontend/components/Appbar/index.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import { NavLink, Link } from 'react-router-dom';
+import { FiTrash } from 'react-icons/fi';
 import { HiChevronRight } from 'react-icons/hi';
 import { RiShoppingCartLine } from 'react-icons/ri';
+import { FaTimes, FaMinus, FaPlus } from 'react-icons/fa';
 import './styles.scss';
 
 const Appbar = () => {
   const [openCart, setOpenCart] = useState(false);
+  const [count, setCount] = useState(1);
+  const price = 20;
 
   return (
     <header className='Appbar'>
@@ -49,14 +53,6 @@ const Appbar = () => {
         >
           Offer
         </NavLink>
-        {/* <NavLink
-          exact
-          to='/contact'
-          activeClassName='active'
-          className='btn-link-gray-700'
-        >
-          Contacto
-        </NavLink> */}
       </nav>
       <div className='Appbar__user flex'>
         {openCart && (
@@ -86,13 +82,76 @@ const Appbar = () => {
         </Link>
       </div>
       {openCart && (
-        <div className='Appbar__cart bounceIn'>
-          <div className='CardCard--min'>
-            <figure className='CardCard__image'>
-              <img src='/food-1.jpg' alt='Food' />
-            </figure>
-            <p className='single-line'>Nombre del producto</p>
-            <p><strong>$20 MXN</strong></p>
+        <div className='Appbar__cart bounceInRight'>
+          <div className='Appbar__cart--header flex items-center justify-between'>
+            <p><strong>Carrito (3)</strong></p>
+            <button
+              type='button'
+              title='Vaciar carrito'
+              className='btn-link-danger'
+            >
+              <FiTrash />
+            </button>
+          </div>
+
+          <div className='Appbar__cart--body content-start scroll-auto'>
+            {[0, 1, 2, 3].map((item) => (
+              <div key={item} className='CardCard--min'>
+                <figure className='CardCard__image'>
+                  <img src='/food-1.jpg' alt='Food' />
+                </figure>
+                <div className='flex items-center justify-between'>
+                  <p className='single-line'>Nombre del producto bien grandote</p>
+                  <button
+                    type='button'
+                    title='Eliminar producto'
+                    className='btn-link-gray'
+                  >
+                    <FaTimes />
+                  </button>
+                </div>
+                <div className='flex items-center justify-between'>
+                  <p><strong>{`$${price * count} MXN`}</strong></p>
+                  <div className='flex items-center'>
+                    <button
+                      type='button'
+                      title='Remover'
+                      disabled={count <= 1}
+                      className='btn-link-gray'
+                      onClick={() => setCount(count - 1)}
+                    >
+                      <FaMinus />
+                    </button>
+                    <p>{count}</p>
+                    <button
+                      type='button'
+                      title='Agreagar'
+                      className='btn-link-accent'
+                      onClick={() => setCount(count + 1)}
+                    >
+                      <FaPlus />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className='Appbar__cart--footer'>
+            <Link
+              to='/cart'
+              onClick={() => setOpenCart(false)}
+              className='form-button btn-link-primary'
+            >
+              Ver carrito
+            </Link>
+            <Link
+              to='/payment'
+              onClick={() => setOpenCart(false)}
+              className='form-button btn-primary'
+            >
+              Proceder al pago
+            </Link>
           </div>
         </div>
       )}
